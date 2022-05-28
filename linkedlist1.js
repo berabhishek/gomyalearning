c57b7b1765651c62160f9e40f53ba2ed6e966d65
berabhishek <bera.abhishek21@gmail.com>
2022-05-28 19:30:17 +0530

    Added append function
    Signed-off-by: berabhishek <bera.abhishek21@gmail.com>

--- linkedlist1.js
+++ linkedlist1.js
@@ -13,6 +13,7 @@ class LinkedList {
             curr.next = new Node(arr[i]);
             curr = curr.next;
         }
+        this.tail = curr;
     }
 
     printList() {
@@ -26,7 +27,22 @@ class LinkedList {
         console.log(str_list);
     }
 
-    appendList(...arr) {
+    length() {
+        let count = 1;
+        let curr = this.head;
+        while (curr !== null) {
+            curr = curr.next;
+            count += 1;
+        }
+        return count;
+    }
+    appendList(...items) {
+        let curr = this.tail;
+        for(let i = 0; i < items.length; i++) {
+            curr.next =  new Node(items[i]);
+            curr = curr.next;
+        }
+        this.tail = curr;
     }
 
     searchElement(target) {
@@ -41,6 +57,58 @@ class LinkedList {
         }
         return -1;
     }
+
+    //deletes the head of LL
+    deleteHead() {
+
+    }
+
+    //deletes the last element of LL
+    deleteTail() {
+
+    }
+
+    //deletes (int)(length / 2) item of the 
+    deleteMiddle() {
+
+    }
+
+    //deletes index-th element
+    deleteFromIndex(index) {
+
+    }
+
+    //detects if LL contains a cycle
+    detectCycle() {
+
+    }
+
+    //removes a cycle from LL, doesn't do anything if no cycle
+    removeCycle() {
+        
+    }
+
+    //reverses a linked list in place
+    reverse() {
+
+    }
+
+    //reverses a LL in groups of k nodes
+    reverseInNode(k) {
+
+    }
+
+    //merge sort the linkedlist
+    sort() {
+
+    }
+
+    //separate the odd and the even numbers
+    oddEven() {
+
+    }
+
+
 }
 
 let ll = new LinkedList([1,2,3,4,5]); 
@@ -48,6 +116,9 @@ let ll = new LinkedList([1,2,3,4,5]);
 console.log(ll.searchElement(7));// -1
 console.log(ll.searchElement(3));// 2
 
+ll.appendList(7); //1,2,3,4,5,7
+ll.appendList(9,10,11,12)// 1,2,3,4,5,7,9,10,11,12
+ll.printList();
 let newll = new LinkedList([4,3,2,1]);
 newll.printList();
 console.log(newll.searchElement(1));
